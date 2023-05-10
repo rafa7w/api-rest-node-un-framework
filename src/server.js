3c40b08fdd01078c47385e8841100ed17d907466
Rafael William <rafael.fim@hotmail.com>
2023-05-10 20:27:59 -0300

    body request in json

--- src/server.js
+++ src/server.js
@@ -6,24 +6,42 @@ import http from 'node:http'
 
 const users = []
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const { method, url } = req
 
+  const buffers = []
+
+  for await (const chunk of req) {
+    buffers.push(chunk)
+  }
+
+  try {
+    // criando uma nova propriedade dentro de req com o nome body
+    req.body = JSON.parse(Buffer.concat(buffers).toString())
+  } catch (error) {
+    req.body = null
+  }
+
+  // GET /users
   if (method === 'GET' && url === '/users') {
     return res
       .setHeader('Content-type', 'application/json')
       .end(JSON.stringify(users))
   }
 
+  // POST /users
   if (method === 'POST' && url === '/users') {
+    const { name, email } = req.body
+
     users.push({
       id: 1,
-      name: 'John Doe',
-      email: 'johndoe@email.com'
+      name: name,
+      email: email
     })
     return res.end('Criação de usuários')
   }
 
+  // GET /
   return res.writeHead(201).end('Hello World')
 })
 
