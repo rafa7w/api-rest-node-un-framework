29cd2d2cab0173b0bfcd520e2999ad41b980c7e5
Rafael William <rafael.fim@hotmail.com>
2023-05-10 20:12:39 -0300

    http methods

--- src/server.js
+++ src/server.js
@@ -4,8 +4,27 @@ Para diferenciar módulos node de terceiros colocar node como prefixo */
 
 import http from 'node:http'
 
+const users = []
+
 const server = http.createServer((req, res) => {
-  return res.end('Hello World')
+  const { method, url } = req
+
+  if (method === 'GET' && url === '/users') {
+    return res
+      .setHeader('Content-type', 'application/json')
+      .end(JSON.stringify(users))
+  }
+
+  if (method === 'POST' && url === '/users') {
+    users.push({
+      id: 1,
+      name: 'John Doe',
+      email: 'johndoe@email.com'
+    })
+    return res.end('Criação de usuários')
+  }
+
+  return res.writeHead(201).end('Hello World')
 })
 
 server.listen(3333)
