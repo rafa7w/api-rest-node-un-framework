da833ab51f0ac7c952d949e555b496308ba93635
Rafael William <rafael.fim@hotmail.com>
2023-05-10 21:35:28 -0300

    persist database

---
+++ src/database.js
@@ -0,0 +1,42 @@
+// fs/promises não têm os métodos de stream => para lidar com stream usar fs nativo
+import fs from 'node:fs/promises'
+
+// Se não estivermos usando o "type": "module" no projeto pode usar o __dirname ou __filename => variáveis do node que contém o path de onde o arquivo foi chamado
+const databasePath = new URL('db.json', import.meta.url)
+
+export class Database {
+  #database = {}
+
+  constructor() {
+    fs.readFile(databasePath, 'urf8')
+      .then(data => {
+        this.#database = JSON.parse(data)
+      })
+      .catch(() => {
+        this.#persist()
+      })
+  }
+
+  #persist() {
+    // esse método do fs só aceita string
+    fs.writeFile(databasePath, JSON.stringify(this.#database))
+  }
+
+  select(table) {
+    const data = this.#database[table] ?? []
+
+    return data
+  }
+
+  insert(table, data) {
+    if (Array.isArray(this.#database[table])) {
+      this.#database[table].push(data)
+    } else {
+      this.#database[table] = [data]
+    }
+
+    this.#persist()
+
+    return data
+  }
+}
