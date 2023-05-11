0418cf3dae076aadd165c287ffbfc7643faefab6
Rafael William <rafael.fim@hotmail.com>
2023-05-11 00:55:19 -0300

    regex and delete method

--- src/server.js
+++ src/server.js
@@ -6,16 +6,30 @@ import http from 'node:http'
 import { routes } from './routes.js'
 import { json } from './middlewares/json.js'
 
+/*
+Exitem três formas do frontend ou qualquer aplicação que esteja consumindo
+nossa API a enviar informações:
+- Query Parameters = parâmetros nomeados localhost:3333/users?userId=1&... URL Stateful,
+filtros, paginação
+
+- Route Parameters =  parâmetros na rota localhost:3333/1 Identificação de recurso
+
+- Request Body = Envio de informações de um formulário
+*/
+
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
 
   await json(req, res)
 
   const route = routes.find(route => {
-    return route.method === method && route.path === url
+    return route.method === method && route.path.test(url)
   })
 
   if (route) {
+    const routeParams = req.url.match(route.path)
+    req.params = { ...routeParams.groups }
+
     return route.handler(req, res)
   }
 
